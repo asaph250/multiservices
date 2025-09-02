e244a5041989dbb0ab118792d366ecb92a0275f2
asaph250 <asaphjackson402@gmail.com>
2025-09-02 10:09:54 +0000

    Fix vite config, update dependencies, build successful

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { componentTagger } from 'lovable-tagger';
 
 export default defineConfig(({ mode }) => ({
-  base: "/multiservices/", // 👈 This is the fix for GitHub Pages
+  base: "/multiservices/", // 👈 GitHub Pages fix
   plugins: [
     react(),
     tsconfigPaths(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  ],
   server: {
     host: "::",
     port: 8080,
